b105d74eedf68583d43d4d9ddc18a2bfadc73272
leduc1901 <leduc1901@github.com>
2020-03-10 21:07:07 +0700

    Updates

---
+++ precache-manifest.4755153459eb144d73e42ef044267e07.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7670f56769e53a89d343328816afeaf9",
+    "url": "/meme-generator/index.html"
+  },
+  {
+    "revision": "75db05c0de97bd789e37",
+    "url": "/meme-generator/static/css/2.1bd2ab2f.chunk.css"
+  },
+  {
+    "revision": "f60f4c4f5db1725a2e37",
+    "url": "/meme-generator/static/css/main.256a11c0.chunk.css"
+  },
+  {
+    "revision": "75db05c0de97bd789e37",
+    "url": "/meme-generator/static/js/2.9b0dbf18.chunk.js"
+  },
+  {
+    "revision": "352b7461bc4eed06983a16fea96ee947",
+    "url": "/meme-generator/static/js/2.9b0dbf18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f60f4c4f5db1725a2e37",
+    "url": "/meme-generator/static/js/main.ae593200.chunk.js"
+  },
+  {
+    "revision": "946f8b2b4ddd05339bb0",
+    "url": "/meme-generator/static/js/runtime-main.27c45a7a.js"
+  }
+]);
